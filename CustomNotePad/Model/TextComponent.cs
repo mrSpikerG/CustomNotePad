fe76341dc1055c5f1f69cd8e416ae8c08caa777e
Fili_vw23@student.itstep.org <Fili_vw23@student.itstep.org>
2022-06-04 22:48:33 +0300

    Добавьте файлы проекта.

---
+++ CustomNotePad/Model/TextComponent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNotePad
+{
+    public class TextComponent : ViewModelBase
+    {
+
+        private string name;
+        private string descr;
+        private string text;
+        private char code;
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TextComponent()
+        {
+            this.Name = String.Empty;
+            this.Description = String.Empty;
+            this.Text = String.Empty;
+            this.Code = '#';
+        }
+        public TextComponent(string name, string description,string text)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Code = this.name[0];
+            this.Text = text;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            set
+            {
+                SetProperty<string>(ref this.name, value);
+                if (name != String.Empty)
+                {
+                    this.Code = this.name[0];
+                }
+                else
+                {
+                    this.Code = '#';
+                }
+                OnPropertyChanged("Name");
+            }
+        }
+        public char Code
+        {
+            get => this.code;
+            set => SetProperty<char>(ref this.code, value);
+        }
+        public string Description
+        {
+            get => this.descr;
+            set => SetProperty<string>(ref this.descr, value);
+        }
+        public string Text
+        {
+            get => this.text;
+            set => SetProperty<string>(ref this.text, value);
+        }
+    }
+}
---
+++ CustomNotePad/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,122 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNotePad
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public static long AllComponents { get; set; }
+        //Models
+        #region
+        private TextComponent selectedModel;
+        public TextComponent SelectedModel
+        {
+            get => this.selectedModel;
+            set
+            {
+                this.selectedModel = value;
+                OnPropertyChanged("SelectedModel");
+            }
+        }
+        public ObservableCollection<TextComponent> CollectionItems { get; set; }
+        #endregion
+        public MainWindowViewModel()
+        {
+            AllComponents = 0;
+            CollectionItems = new ObservableCollection<TextComponent>();
+            ReadFromFile();
+            if (!Directory.Exists("Data"))
+            {
+                Directory.CreateDirectory("Data");
+            }
+
+        }
+
+
+
+
+
+        //Commands
+        #region
+        //AddCommand
+        #region
+        private RelayCommand addCommand;
+
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ?? (addCommand = new RelayCommand(() =>
+                {
+                    TextComponent text = new TextComponent();
+                    CollectionItems.Add(text);
+                    SelectedModel = text;
+                }));
+            }
+            set { addCommand = value; }
+        }
+        #endregion
+
+        //DeleteCommand
+        #region 
+        private RelayCommand deleteCommand;
+
+        public RelayCommand DeleteCommand
+        {
+            get
+            {
+                return deleteCommand ?? (deleteCommand = new RelayCommand(() =>
+                {
+                    CollectionItems.Remove(SelectedModel);
+                }));
+            }
+            set { addCommand = value; }
+        }
+        #endregion
+        #endregion
+
+
+        //Save & Read
+        #region
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            for (int i = 0; i < this.CollectionItems.Count; i++)
+            {
+                File.WriteAllText($"Data/{i}.txt", String.Format("{0}|{1}|{2}", this.CollectionItems[i].Name, this.CollectionItems[i].Description, this.CollectionItems[i].Text));
+            }
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void ReadFromFile()
+        {
+            string[] files = Directory.GetFiles("Data");
+            for (int i = 0; i < files.Length; i++)
+            {
+                try
+                {
+                    string TEMP = File.ReadAllText(files[i]);
+                    string[] TEMP2 = TEMP.Split("|");
+                    this.CollectionItems.Add(new TextComponent(TEMP2[0], TEMP2[1], TEMP2[2]));
+                    AllComponents++;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        #endregion
+    }
+}
